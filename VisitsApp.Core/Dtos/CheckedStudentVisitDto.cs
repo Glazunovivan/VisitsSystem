beb32c3cdd6af104a6eb03325f8b77cb315bd1cc
iglazunov <ivan.glaz.2029@mail.ru>
2026-03-02 11:27:44 +0300

    #4 Мультивыбор в посещаемости

---
+++ VisitsApp.Core/Dtos/CheckedStudentVisitDto.cs
@@ -0,0 +1,16 @@
+﻿using VisitsApp.Core.Models;
+
+namespace VisitsApp.Core.Dtos
+{
+    public class CheckedStudentVisitDto
+    {
+        public Student Model { get; set; }
+
+        public bool IsChecked { get; set; } 
+
+        public CheckedStudentVisitDto(Student model)
+        {
+            Model = model;   
+        }
+    }
+}
